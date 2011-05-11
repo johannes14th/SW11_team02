2147a8acca7454496260169c586e525596204735
Christof Steinkellner <csteinkellner@student.tugraz.at>
2011-05-11 13:48:12 +0200

    view pdf

--- src/viewPdf/TestViewPdf.java
+++ src/viewPdf/TestViewPdf.java
@@ -8,12 +8,22 @@ import junit.framework.TestCase;
 
 public class TestViewPdf extends TestCase {
 	ViewPdf test = new ViewPdf();
+
 	
 	public void testPath(){
+		File file = new File("C:" + File.separator + "pdfs" + File.separator + "test.pdf");
+		test.setFileName(file);
 		File filename = test.getFileName();
 		boolean check = filename.exists();
 		assertEquals(true, check);
 	}
 	
+	public void testString(){
+		File file = new File("C:" + File.separator + "pdfs" + File.separator + "test.pdf");
+		test.setFileName(file);
+		File filename = new File(test.getFileNameString());
+		boolean check = filename.exists();
+		assertEquals(true, check);
+	}
 	
 }
--- src/viewPdf/ViewPdf.java
+++ src/viewPdf/ViewPdf.java
@@ -2,14 +2,24 @@ package viewPdf;
 
 import java.io.File;
 
+
 public class ViewPdf {
+	File filename;
 	
-	
+	public void setFileName(File file){
+		filename = file;
+	}
 	
 	public File getFileName() {
-	File filename = new File("C:" + File.separator + "pdfs" + File.separator + "test.pdf");//pfad?
+		//filename = new File("C:" + File.separator + "pdfs" + File.separator + "test.pdf");//pfad?
 		return filename;
 	}
+	
+	public String getFileNameString(){
+		return filename.toString();
+	}
+	   
+	
 	
 	
 
